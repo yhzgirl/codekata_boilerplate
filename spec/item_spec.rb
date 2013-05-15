9c3ee01742c9c0114c1865b9dcd9064ec1987516
'Michelle' <'yhzgirl@gmail.com'>
2013-05-15 22:14:55 +0100

    fist commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require '/item'
+
+describe 'item' do
+	context '#imported?' do
+		it 'returns true if item is imported' do
+		end
+		it 'returns false if item is not imported' do
+		end
+	end
+end
